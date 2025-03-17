e112f1d483c8355b2b81ba1a942d003a9e3ef23d
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2025-03-17 15:21:04 +0800

    update serializer user view

--- api/serializers_review.py
+++ api/serializers_review.py
@@ -1,8 +1,20 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
+from rest_framework import serializers
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Pastikan akun aktif
+        user = self.user
+        if not user.is_active:
+            raise serializers.ValidationError("Akun tidak aktif. Hubungi admin.")
+
+        # Tambahkan informasi tambahan dalam token
+        data.update({'username': user.username, 'email': user.email})
+        return data
+    
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
